4f3870e5a8b6ab0e9bd4bc976c5a9e6c4a7e98f8
Hugo Duncan <hugo@hugoduncan.org>
2013-03-20 05:42:45 -0700

    Inital version for cw

---
+++ code/project.clj
@@ -0,0 +1,7 @@
+(defproject code "0.1.0-SNAPSHOT"
+  :description "Code for Ritz presentation at clojure/west 2013"
+  :url "https://github.com/hugoduncan/ritz-conj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.macro "0.1.1"]])
---
+++ code/src/ritz_cw/example.clj
@@ -0,0 +1,81 @@
+(ns ritz-cw.example
+  ;; (:require [])
+  )
+
+
+;;; simple evaluation with exceptions
+
+;; Default - no popup in repl
+;; (setq nrepl-popup-stacktraces t)
+;; (setq nrepl-popup-stacktraces-in-repl nil)
+
+;; Popup stack trace
+;; (setq nrepl-popup-stacktraces-in-repl t)
+
+#_ (/ 1 0)
+
+;; Stop stack unwinding
+;; M-x nrepl-ritz-break-on-exception
+
+;; lein pom
+;; mvn dependency:sources
+
+
+(defn f1 []
+  (let [a 1]
+    (/ a 0)))
+
+(comment (f1))
+
+(defn f5 []
+  (let [c (range 10)]
+    (/ (first c) 0)))
+
+(comment (f5))
+
+
+;;; Tracking down errors
+(comment
+  (defmulti fred [x] x))
+
+(comment
+  (ns fred
+    (:use
+     [clojure.string :only join])))
+
+
+(defn f2 []
+  (let [a (map inc (range 1000))]
+    (/ (first a) 0)))
+
+;; (comment (f2))
+
+;;; Lazy sequences
+(defn f3 []
+  (let [a (range 10000)]
+    (/ (first a) 0)))
+
+;; (comment (f3))
+
+
+;;; Locals clearing
+(defn f4 []
+  (let [b (range 10)
+        c (map inc b)]
+    (f1)))
+
+;; (comment (f4))
+
+;; (defn String capitalise [s]
+;;   s)
+
+
+
+
+
+
+
+(defn get-some-key [m]
+  (select-keys m :some-key))
+
+;; (get-some-key {})
